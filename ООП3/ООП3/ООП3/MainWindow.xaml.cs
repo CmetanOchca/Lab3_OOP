1bb2b1f5ffacad55b37a2de2933d4a77ce8de191
vanotuvanov <ivan.tumanov.1999@mail.ru>
2022-05-24 17:52:21 +0300

    работает

--- ООП3/ООП3/ООП3/MainWindow.xaml.cs
+++ ООП3/ООП3/ООП3/MainWindow.xaml.cs
@@ -15,13 +15,15 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ООП3
-{
-
+{ 
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DoubleAnimation buttonAnimation = new DoubleAnimation();
+        int countPositionX = 0;
+        int countPositionY = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,32 +31,62 @@ namespace ООП3
 
         private void OnUp(object sender, RoutedEventArgs e)
         {
-
-
+            if (countPositionY < 10)
+            {
+                countPositionY++;
+                buttonAnimation.From = Line.ActualHeight;
+                buttonAnimation.By = -10;
+                buttonAnimation.Duration = TimeSpan.FromSeconds(0);
+                Line.BeginAnimation(Button.HeightProperty, buttonAnimation);
+                ThicknessAnimation thicknessAnimation = new ThicknessAnimation()
+                {
+                    From = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                    To = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top - 10, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                    Duration = TimeSpan.FromSeconds(0),
+                };
+                Magnet.BeginAnimation(MarginProperty, thicknessAnimation);
+            }
         }
+
 
         private void OnRight(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation buttonAnimation = new DoubleAnimation();
-            buttonAnimation.From = Holder.ActualWidth;
-            buttonAnimation.To = 100;
-            buttonAnimation.Duration = TimeSpan.FromSeconds(0);
-            Holder.BeginAnimation(Button.WidthProperty, buttonAnimation);
+            if (countPositionX < 10)
+            {
+                countPositionX++;
+                buttonAnimation.From = Holder.ActualWidth;
+                buttonAnimation.By = 35;
+                buttonAnimation.Duration = TimeSpan.FromSeconds(0);
+                Holder.BeginAnimation(Button.WidthProperty, buttonAnimation);
+            }
         }
 
         private void OnLeft(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation buttonAnimation = new DoubleAnimation();
             buttonAnimation.From = Holder.ActualWidth;
-            buttonAnimation.To = 50;
+            buttonAnimation.By = -35;
             buttonAnimation.Duration = TimeSpan.FromSeconds(0);
             Holder.BeginAnimation(Button.WidthProperty, buttonAnimation);
-
+            if (countPositionX>0) { countPositionX--; }
         }
 
         private void OnDown(object sender, RoutedEventArgs e)
         {
-
+            if (countPositionY > -10)
+            {
+                countPositionY--;
+                buttonAnimation.From = Line.ActualHeight;
+                buttonAnimation.By = 10;
+                buttonAnimation.Duration = TimeSpan.FromSeconds(0);
+                Line.BeginAnimation(Button.HeightProperty, buttonAnimation);
+                ThicknessAnimation thicknessAnimation = new ThicknessAnimation()
+                {
+                    From = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                    To = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top + 10, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                    Duration = TimeSpan.FromSeconds(0),
+                };
+                Magnet.BeginAnimation(MarginProperty, thicknessAnimation);
+            }
         }
 
         private void OnTake(object sender, RoutedEventArgs e)
