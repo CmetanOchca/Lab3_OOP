49a2e0c6b5bdfae6d7348641d10dae7d8cb16bdf
Viktoria <Victoriakirsanov@mail.ru>
2022-05-27 15:03:49 +0300

    v 0.4
    пофиксили катание груза по земле

--- ООП3/ООП3/ООП3/MainWindow.xaml.cs
+++ ООП3/ООП3/ООП3/MainWindow.xaml.cs
@@ -21,7 +21,6 @@ namespace ООП3
     /// </summary>
     public partial class MainWindow : Window
     {
-        DoubleAnimation buttonAnimation = new DoubleAnimation();
         private int countPositionX = 0;
         private int countPositionY = 0;
         private float speedAnimation = 0;
@@ -37,50 +36,43 @@ namespace ООП3
             if (countPositionY < 10)
             {
                 countPositionY++;
-                //buttonAnimation.From = Line.ActualHeight;
-                //buttonAnimation.By = -10;
-                //buttonAnimation.Duration = TimeSpan.FromSeconds(speedAnimation);
-                //Line.BeginAnimation(Button.HeightProperty, buttonAnimation);
                 Line.SetValue(HeightProperty, Convert.ToDouble(Line.GetValue(HeightProperty)) - 10.0);
-                ThicknessAnimation thicknessAnimation = new ThicknessAnimation()
-                {
-                    From = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top, Magnet.Margin.Right, Magnet.Margin.Bottom),
-                    To = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top - 10, Magnet.Margin.Right, Magnet.Margin.Bottom),
-                    Duration = TimeSpan.FromSeconds(speedAnimation),
-                };
-                Magnet.BeginAnimation(MarginProperty, thicknessAnimation);
+                //ThicknessAnimation thicknessAnimation = new ThicknessAnimation()
+                //{
+                //    From = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                //    To = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top - 10, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                //    Duration = TimeSpan.FromSeconds(speedAnimation),
+                //};
+                //Magnet.BeginAnimation(MarginProperty, thicknessAnimation);
                 if (TakeCargo) Cargo.SetValue(Canvas.TopProperty, Convert.ToDouble(Cargo.GetValue(Canvas.TopProperty)) - 10.0);
-               
-                //Magnet.SetValue(Canvas.TopProperty, Convert.ToDouble(Magnet.GetValue(TopProperty)) -10.0);
             }
         }
 
-
         private void OnRight(object sender, RoutedEventArgs e)
         {
-            if (countPositionX < 5)
+            if ((TakeCargo) && (countPositionY == -10)) { }
+            else
             {
-                countPositionX++;
-                //buttonAnimation.From = Holder.ActualWidth;
-                //buttonAnimation.By = 35;
-                //buttonAnimation.Duration = TimeSpan.FromSeconds(speedAnimation);
-                //Holder.BeginAnimation(Button.WidthProperty, buttonAnimation);
-                Holder.SetValue(Canvas.LeftProperty, Convert.ToDouble(Holder.GetValue(LeftProperty)) + 35.0);
-                if (TakeCargo) Cargo.SetValue(Canvas.LeftProperty, Convert.ToDouble(Cargo.GetValue(Canvas.LeftProperty)) + 35.0);
+                if (countPositionX < 5)
+                {
+                    countPositionX++;
+                    Holder.SetValue(Canvas.LeftProperty, Convert.ToDouble(Holder.GetValue(LeftProperty)) + 35.0);
+                    if (TakeCargo) Cargo.SetValue(Canvas.LeftProperty, Convert.ToDouble(Cargo.GetValue(Canvas.LeftProperty)) + 35.0);
+                }
             }
         }
 
         private void OnLeft(object sender, RoutedEventArgs e)
         {
-            if (countPositionX > 0)
+            if ((TakeCargo) && (countPositionY == -10)) { }
+            else
             {
-                countPositionX--;
-                //buttonAnimation.From = Holder.ActualWidth;
-                //buttonAnimation.By = -35;
-                //buttonAnimation.Duration = TimeSpan.FromSeconds(speedAnimation);
-                //Holder.BeginAnimation(Button.WidthProperty, buttonAnimation);
-                Holder.SetValue(Canvas.LeftProperty, Convert.ToDouble(Holder.GetValue(LeftProperty)) - 35.0);
-                if (TakeCargo) Cargo.SetValue(Canvas.LeftProperty, Convert.ToDouble(Cargo.GetValue(Canvas.LeftProperty)) - 35.0);
+                if (countPositionX > 0)
+                {
+                    countPositionX--;
+                    Holder.SetValue(Canvas.LeftProperty, Convert.ToDouble(Holder.GetValue(LeftProperty)) - 35.0);
+                    if (TakeCargo) Cargo.SetValue(Canvas.LeftProperty, Convert.ToDouble(Cargo.GetValue(Canvas.LeftProperty)) - 35.0);
+                }
             }
         }
 
@@ -89,30 +81,22 @@ namespace ООП3
             if (countPositionY > -10)
             {
                 countPositionY--;
-                //buttonAnimation.From = Line.ActualHeight;
-                //buttonAnimation.By = 10;
-                //buttonAnimation.Duration = TimeSpan.FromSeconds(speedAnimation);
-                //Line.BeginAnimation(Button.HeightProperty, buttonAnimation);
                 Line.SetValue(HeightProperty, Convert.ToDouble(Line.GetValue(HeightProperty)) + 10.0);
-                ThicknessAnimation thicknessAnimation = new ThicknessAnimation()
-                {
-                    From = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top, Magnet.Margin.Right, Magnet.Margin.Bottom),
-                    To = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top + 10, Magnet.Margin.Right, Magnet.Margin.Bottom),
-                    Duration = TimeSpan.FromSeconds(speedAnimation),
-                };
-                Magnet.BeginAnimation(MarginProperty, thicknessAnimation);
+                //ThicknessAnimation thicknessAnimation = new ThicknessAnimation()
+                //{
+                //    From = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                //    To = new Thickness(Magnet.Margin.Left, Magnet.Margin.Top + 10, Magnet.Margin.Right, Magnet.Margin.Bottom),
+                //    Duration = TimeSpan.FromSeconds(speedAnimation),
+                //};
+                //Magnet.BeginAnimation(MarginProperty, thicknessAnimation);
                 if (TakeCargo) Cargo.SetValue(Canvas.TopProperty, Convert.ToDouble(Cargo.GetValue(Canvas.TopProperty)) + 10.0);
-
-
             }
         }
 
         private void OnTake(object sender, RoutedEventArgs e)
         {
-            double HolderX;
-            double CargoX;
-            HolderX = Convert.ToDouble(Holder.GetValue(LeftProperty));
-            CargoX = Convert.ToDouble(Cargo.GetValue(LeftProperty));
+            double HolderX = Convert.ToDouble(Holder.GetValue(LeftProperty));
+            double CargoX = Convert.ToDouble(Cargo.GetValue(LeftProperty));
             if ((HolderX == CargoX) && (countPositionY == -10) && (!TakeCargo))
             {
                 TakeCargo = true;
@@ -124,7 +108,6 @@ namespace ООП3
                 TakeCargo = false; 
                 Take.Background = Brushes.White; 
             }
-
         }
     }
 }
